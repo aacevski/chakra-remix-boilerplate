228cd60d4f3f6b06c8f2ec785550c412f4c4c808
Andrej Acevski <aacevski@gmail.com>
2022-03-09 11:17:58 +0100

    ⚡

---
+++ app/entry.server.tsx
@@ -0,0 +1,44 @@
+import { renderToString } from 'react-dom/server';
+import { CacheProvider } from '@emotion/react';
+import createEmotionServer from '@emotion/server/create-instance';
+import type { EntryContext } from 'remix';
+import { RemixServer } from 'remix';
+import 'dotenv/config';
+
+import { ServerStyleContext } from './context';
+import { createEmotionCache } from './createEmotionCache';
+
+export default function handleRequest(
+  request: Request,
+  responseStatusCode: number,
+  responseHeaders: Headers,
+  remixContext: EntryContext
+) {
+  const cache = createEmotionCache();
+  const { extractCriticalToChunks } = createEmotionServer(cache);
+
+  const html = renderToString(
+    <ServerStyleContext.Provider value={null}>
+      <CacheProvider value={cache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </CacheProvider>
+    </ServerStyleContext.Provider>
+  );
+
+  const chunks = extractCriticalToChunks(html);
+
+  const markup = renderToString(
+    <ServerStyleContext.Provider value={chunks.styles}>
+      <CacheProvider value={cache}>
+        <RemixServer context={remixContext} url={request.url} />
+      </CacheProvider>
+    </ServerStyleContext.Provider>
+  );
+
+  responseHeaders.set('Content-Type', 'text/html');
+
+  return new Response(`<!DOCTYPE html>${markup}`, {
+    status: responseStatusCode,
+    headers: responseHeaders,
+  });
+}
---
+++ app/root.tsx
@@ -0,0 +1,135 @@
+import React from 'react';
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
+  useCatch,
+} from 'remix';
+import type { MetaFunction } from 'remix';
+import { VStack, Heading, ChakraProvider, Text } from '@chakra-ui/react';
+import { withEmotionCache } from '@emotion/react';
+
+import { ServerStyleContext, ClientStyleContext } from './context';
+
+export const meta: MetaFunction = () => {
+  return { title: 'Chakra UI Boilerplate' };
+};
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  );
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  console.error(error);
+  return (
+    <Document>
+      <VStack h="100vh" justify="center">
+        <Heading>There was an error</Heading>
+        <Text>{error.message}</Text>
+        <hr />
+        <Text>
+          Hey, developer, you should replace this with what you want your users
+          to see.
+        </Text>
+      </VStack>
+    </Document>
+  );
+}
+
+export function CatchBoundary() {
+  let caught = useCatch();
+  let message;
+  switch (caught.status) {
+    case 401:
+      message = (
+        <Text>
+          Oops! Looks like you tried to visit a page that you do not have access
+          to.
+        </Text>
+      );
+      break;
+    case 404:
+      message = (
+        <Text>
+          Oops! Looks like you tried to visit a page that does not exist.
+        </Text>
+      );
+      break;
+
+    default:
+      throw new Error(caught.data || caught.statusText);
+  }
+
+  return (
+    <Document>
+      <VStack h="100vh" justify="center">
+        <Heading>
+          {caught.status}: {caught.statusText}
+        </Heading>
+        {message}
+      </VStack>
+    </Document>
+  );
+}
+
+interface DocumentProps {
+  children: React.ReactNode;
+}
+
+const Document = withEmotionCache(
+  ({ children }: DocumentProps, emotionCache) => {
+    const serverSyleData = React.useContext(ServerStyleContext);
+    const clientStyleData = React.useContext(ClientStyleContext);
+
+    // Only executed on client
+    React.useEffect(() => {
+      // re-link sheet container
+      emotionCache.sheet.container = document.head;
+      // re-inject tags
+      const tags = emotionCache.sheet.tags;
+      emotionCache.sheet.flush();
+      tags.forEach((tag) => {
+        (emotionCache.sheet as any)._insertTag(tag);
+      });
+      // reset cache to reapply global styles
+      clientStyleData?.reset();
+    }, []);
+
+    return (
+      <html lang="en">
+        <head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstaticom" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
+            rel="stylesheet"
+          />
+          <Meta />
+          <Links />
+          {serverSyleData?.map(({ key, ids, css }) => (
+            <style
+              key={key}
+              data-emotion={`${key} ${ids.join(' ')}`}
+              dangerouslySetInnerHTML={{ __html: css }}
+            />
+          ))}
+        </head>
+        <body>
+          <ChakraProvider>{children}</ChakraProvider>
+          <ScrollRestoration />
+          <Scripts />
+          {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
+        </body>
+      </html>
+    );
+  }
+);
